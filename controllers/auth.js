e29ee33ffecc4b41f1caf89af5ab80a7785a5084
Josh Taylor <taylorj12@coventry.ac.uk>
2023-02-20 19:46:52 +0000

    currently partially implemented jwt authentication

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,6 +1,13 @@
 const passport = require('koa-passport');
 const basicAuth = require('../strategies/basic');
+const [...params] = require('../strategies/jwt')
+const JwtStrategy = require('passport-jwt').Strategy,
+ExtractJwt = require('passport-jwt').ExtractJwt;
 
-passport.use(basicAuth);
 
-module.exports = passport.authenticate(['basic'], {session:false});
+
+passport.use(new JwtStrategy(params[1], params[0]));
+
+module.exports = passport.authenticate('jwt', {session:false}), function(req, res) {
+  res.send(req.user.profile);
+};
