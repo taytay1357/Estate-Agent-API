7a61e22856a456f8ee208e811fc1dd8f6c5ac4ab
Josh Taylor <taylorj12@coventry.ac.uk>
2023-03-10 15:21:30 +0000

    added permissions to whole system now all resources are protected just need to perform testing with postman

---
+++ permissions/property.js
@@ -0,0 +1,23 @@
+const AccessControl = require('role-acl');
+const ac = new AccessControl();
+
+
+ac.grant('agent').condition({Fn: 'EQUALS', args: {'requester': '$.owner'}}).execute('update')
+   .on('property', ['name', 'price', 'address', 'bedrooms', 'bathrooms', 'description','imageURL']);
+
+ac.grant('agent').execute('create').on('property', ['name', 'price', 'address', 'bedrooms', 'bathrooms', 'description','imageURL']);
+
+ac.grant('admin').execute('read').on('property');
+ac.grant('admin').execute('read').on('properties');
+ac.grant('admin').execute('update').on('property');
+ac.grant('agent').condition({Fn: 'EQUALS', args: {'requester': '$.owner'}}).execute('delete').on('property');
+
+
+exports.create = (requester) =>
+   ac.can(requester.role).execute('create').sync().on('properties');
+
+exports.update = (requester, data) => 
+   ac.can(requester.role).context({requester:requester.sub, owner:data.ID}).execute('update').sync().on('property');
+
+exports.delete = (requester, data) =>
+   ac.can(requester.role).context({requester:requester.sub, owner:data.ID}).execute('delete').sync().on('property')
--- routes/properties.js
+++ routes/properties.js
@@ -5,7 +5,8 @@ const model = require('../models/properties');
 const bodyParser = require('koa-bodyparser')
 const {validateProperty} = require('../controllers/validation')
 const router = Router({prefix: '/api/v1/properties'});
-
+const can = require('../permissions/property');
+const jwtUtils = require('../helpers/jsonwebtoken');
 
 router.get('/', getAll);
 router.post('/', bodyParser(), validateProperty ,createProperty);
@@ -26,42 +27,67 @@ async function getAll(cnx) {
 async function getById(cnx) {
   //Get the ID from the route parameters.
   let id = cnx.params.id
-  let users = await model.getById(id);
-  if (users.length) {
-    cnx.body = users[0];
+  let properties = await model.getById(id);
+  if (properties.length) {
+    cnx.body = properties[0];
   }
 }
 
 async function createProperty(cnx) {
-  const body = cnx.request.body;
-  let result = await model.add(body)
-  if (result) {
-    cnx.status = 201;
-    cnx.body = {ID: result.insertId}
+  const jwt = cnx.request.header.authorization;
+  const payload = jwtUtils.decodeJWT(jwt);
+  const permission = can.create(payload);
+  if (!permission.granted) {
+    cnx.status = 403;
+  } else {
+    const body = cnx.request.body;
+    body.agentID = payload.sub
+    let result = await model.add(body)
+    if (result) {
+      cnx.status = 201;
+      cnx.body = {msg: 'property inserted into database'}
+    }
   }
 }
 
 async function updateProperty(cnx) {
+  const jwt = cnx.request.header.authorization;
+  const payload = jwtUtils.decodeJWT(jwt);
   //first of all get the id of the article
   let id = cnx.params.id
-  //receive request body and assign it to a new article variable
-  let {type, price, address, bedrooms, bathrooms, agentId, description, imageURL} = cnx.request.body;
-  let updatedProperty = {type: type, price: price, address: address, bedrooms: bedrooms, bathrooms: bathrooms, agentId: agentId, imageURL: imageURL, description: description}
-  let result = await model.update(updatedProperty, id)
-  if (result) {
-    cnx.status = 201;
-    cnx.body = {ID: result.insertId}
+  id = {ID: id};
+  const permission = can.update(payload, id);
+  if (!permission.granted) {
+    cnx.status = 403;
+  } else {
+    //receive request body and assign it to a new article variable
+    let {type, price, address, bedrooms, bathrooms, description, imageURL} = cnx.request.body;
+    let updatedProperty = {type: type, price: price, address: address, bedrooms: bedrooms, bathrooms: bathrooms, imageURL: imageURL, description: description}
+    let result = await model.update(updatedProperty, id.ID)
+    if (result) {
+      cnx.status = 201;
+      cnx.body = {msg: 'record has been updated'}
+    }
   }
 }
 
 
 
 async function deleteProperty(cnx) {
+  const jwt = cnx.request.header.authorization;
+  const payload = jwtUtils.decodeJWT(jwt);
   //first get the id of the article we want to delete
   let id = cnx.params.id
-  let result = await model.delete(id)
-  if (result) {
-    cnx.status = 201;
+  id = {ID: id};
+  const permission = can.delete(payload, id);
+  if (!permission.granted) {
+    cnx.status = 403;
+  } else {
+    let result = await model.delete(id.ID)
+    if (result) {
+      cnx.status = 201;
+      cnx.body = {msg: 'record has been deleted'}
+    }
   }
 }
 
