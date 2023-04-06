1309d85347d3e4a809690a6c3c44edc893993215
Josh Taylor <taylorj12@coventry.ac.uk>
2023-04-06 07:40:53 +0000

    logging and overall project now complete

--- estate-front/src/front_helper/helper.js
+++ estate-front/src/front_helper/helper.js
@@ -1,5 +1,17 @@
-import decodeJWT from './jwt_helper'
+/**
+ * A module to get jwts from local storage.
+ * @module estate-front/src/front_helper/helper/
+ * @author Josh Taylor
+ * @see routes/* for the routes that require this module
+ */
 
+
+import decodeJWT from './jwt_helper'
+/**
+ * A function which gets a jwt and returns it
+ * @param {function} setLoggedIn - this is state that is set if jwt has expired
+ * @returns {jwt} - the jwt token
+ */
 export default function getFromLocal(setLoggedIn) {
    let jwt = localStorage.getItem('jwt');
    if (jwt !== undefined && jwt) {
--- estate-front/src/front_helper/jwt_helper.js
+++ estate-front/src/front_helper/jwt_helper.js
@@ -1,7 +1,19 @@
+/**
+ * A module to get jwts from local storage.
+ * @module estate-front/src/front_helper/jwt_helper/
+ * @author Josh Taylor
+ * @see routes/* for the routes that require this module
+ */
+
+
 import { Buffer } from 'buffer';
 const base64 = require('base64url');
 
-
+/**
+ * A module that decodes a given jwt and returns its payload
+ * @param {string} jwt - the jwt the user has given as a header
+ * @returns {object} the payload that has been decoded from the jwt
+ */
 export default function decodeJWT(signedJWT) {
    const payload = signedJWT.split('.')[1];
    //now decode it
